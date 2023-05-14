cc75615997118080be55a2fb113073b88da4b7d2
PabloChristianno <pablochristianno@gmail.com>
2023-05-14 10:30:03 -0300

    Começando solução do problema

--- MaximumProbabilityPath.py
+++ MaximumProbabilityPath.py
@@ -1,1 +1,29 @@
 # Path with Maximum Probability(Caminho com Maior Probabilidade)
+import heapq
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        graph = collections.defaultdict(list)
+        for i in range(len(edges)):
+            p, s = edges[i]
+            graph[p].append((succProb[i], s))
+            graph[s].append((succProb[i], p))
+        
+        # Declarando a maxheap
+        max_heap = [(-1, start)]
+        visited = []
+        
+        while max_heap:
+            # Remove da heap
+            p, vertice = heapq.heappop(max_heap)  # p < 0
+            
+            if vertice == end:
+                return p
+            
+            visited.append(vertice)
+            for i, j in graph[vertice]:
+                if j not in visited:
+                    # Adiciona na heap
+                    heapq.heappush(max_heap, ((p*i), j))
+                
+        return 0.00000
